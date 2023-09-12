ebf7f15fea5a22426fa83cd44b9f3c652d13190e
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-09-11 23:30:44 -0400

    Add file selection mechanism (regex!)

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import os
+import os, re
 
-os.system("echo test")
+ABS_PATH = os.path.dirname(__file__)
+MULTIFY_PATH = os.path.join(ABS_PATH, "panda3d", "multify.exe")
+TARGET_FILE_PATH = os.path.join(ABS_PATH, "test", "example_files")
+
+def isTargetFile(fp):
+    #regex matching only files that can be unlocked and edited
+    #overcomplicated, but i like regex and it allows for capture of mf name later
+    return re.search("(phase_[\d\.]+_(?!models|dna|paths|luts|shaders).*)\.mf", fp)
+
+fileList: list = os.listdir(TARGET_FILE_PATH)
+targetFileList = list(filter(isTargetFile, fileList))
+
+#os.system(f"{MULTIFY_PATH} -x -f {os.path.join(TARGET_FILE_PATH, fileList[0])}")
