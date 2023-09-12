e83c32e907ef1f5fad039b38b047af69f9d6d8c7
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-09-12 19:39:04 -0400

    Convert to reusable module

--- packer.py
+++ packer.py
@@ -1,57 +1,67 @@
-import os, re, shutil, time
+import os, re, shutil, time, subprocess
 
-ABS_PATH = os.path.dirname(__file__)
-MULTIFY_PATH = os.path.join(ABS_PATH, "panda3d", "multify.exe")
-TARGET_FILE_PATH = os.path.join(ABS_PATH, "test", "example_files")
-DESTINATION_PATH = os.path.join(ABS_PATH, "test", "example_output")
+CWD_PATH = os.path.dirname(__file__)
+MULTIFY_PATH = os.path.join(CWD_PATH, "panda3d", "multify.exe")
+DEFAULT_TARGET_FILE_PATH = os.path.join(CWD_PATH, "test", "example_files")
+DEFAULT_DESTINATION_PATH = os.path.join(CWD_PATH, "test", "example_output")
 
-def isTargetFile(fp: str) -> bool:
+def _isTargetFile(fp: str) -> bool:
     """
     Returns whether the target file is a phase file that can be unlocked and edited via a regex.
     Allows for capture of phase file name.
     """
     return re.search("(phase_[\d\.]+_(?!models|dna|paths|luts|shaders).*)\.mf", fp)
 
-def isTargetDir(dp: str) -> bool:
+def _isTargetDir(dp: str) -> bool:
     """
     Returns whether the target directory is a phase file directory via a regex.
     Allows for capture of phase file name.
     """
     return re.search("^(phase_\d\.?\d?)$", dp)
 
-fileList: list = os.listdir(TARGET_FILE_PATH)
-targetFileList = filter(isTargetFile, fileList)
-
-def multify(filename: str):
+def _multify_file(dir: str, filename: str, target_dir: str=CWD_PATH):
     """
     Uses the multify tool to unpack a Panda3D multifile.
+    By default, leaves phase_X folders in the current directory. target_dir changes this behaviour.
     """
     start = time.time()
-    os.system(f"{MULTIFY_PATH} -x -f {os.path.join(TARGET_FILE_PATH, filename)}")
+    subprocess.run([MULTIFY_PATH, "-x", "-f", os.path.join(dir, filename)], shell=True, cwd=target_dir)
     end = time.time()    
-    print(f"Multified {filename}! \t took {round(end-start, 2)}s")
+    print(f"Unpacked {filename}! \t took {round(end-start, 2)}s")
 
-for file in targetFileList:
-    #multify(file)
-    pass
+def unpackDirectory(dir: str = DEFAULT_TARGET_FILE_PATH, target_dir: str=CWD_PATH) -> None:
+    """
+    Uses the multify tool to unpack all Panda3D multifiles in a directory.
+    By default, leaves phase_X folders in the current directory. target_dir changes this behaviour.
+    """
+    fileList: list = os.listdir(dir)
+    targetFileList = filter(_isTargetFile, fileList)
+    for file in targetFileList:
+        _multify_file(dir, file, target_dir)
 
-folderList = os.listdir(ABS_PATH)
-targetMoveList = filter(isTargetDir, folderList)
-
-def checkTargetEmpty(targetDir) -> bool:
+def _checkTargetEmpty(targetDir) -> bool:
     """
     Ensures that phase directories will not get overwritten.
     """
     objsInDir = os.listdir(targetDir)
     for obj in objsInDir:
-        if isTargetDir(obj):
-            print(f"{obj} was detected as a possible existing phase file! Exiting to avoid overwrite...")
-            exit()
+        if _isTargetDir(obj):
+            print(f"Warning: {obj} was detected as a possible existing phase file!")
+            return False
     return True
 
-ok = checkTargetEmpty(DESTINATION_PATH)
+def movePhaseToDirectory(fromPath=CWD_PATH, toPath=DEFAULT_DESTINATION_PATH) -> None:
+    folderList = os.listdir(fromPath)
+    targetMoveList = filter(_isTargetDir, folderList)
+    
+    if _checkTargetEmpty(toPath):
+        for dir in targetMoveList:
+            print(f"Moving: {str.rjust('/'+dir+'/', 12)} to /{toPath}/ ...", end="")
+            shutil.move(dir, toPath)
+            print(" done.")
+    else: print("Aborting to avoid overwrite...")
 
-for dir in targetMoveList:
-    print(f"Moving: {str.rjust('/'+dir+'/', 12)} to /{DESTINATION_PATH}/ ...", end="")
-    shutil.move(dir, DESTINATION_PATH)
-    print(" done.")
+#unpackDirectory()
+#movePhaseToDirectory()
+
+#TODO: re-multify function
