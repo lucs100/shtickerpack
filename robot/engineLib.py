e6e2e1ed266030e92e135bc88a285752fb9ccff9
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-10-22 02:39:38 -0400

    Impl repack wrapper keyword

--- robot/engineLib.py
+++ robot/engineLib.py
@@ -1,6 +1,21 @@
+import os
+import sys
+
+rootpath = os.path.abspath(f"C:/Users/{os.getlogin()}/Documents/projects/Python/shtickerpack")
+sys.path.append(rootpath)
 
 import engine
 
+DEFAULT_LOOSE_FILES_DIR = f"C:/Users/{os.getlogin()}/Documents/projects/Python/shtickerpack/test/loose_files"
+DEFAULT_CONTENTPACKS_DIR = f"C:/Users/{os.getlogin()}/AppData/Local/Corporate Clash/resources/contentpacks"
+
 class engineLib(object):
-    ROBOT_LIBRARY_VERSION = __version__
-    ROBOT_LIBRARY_VERSION = "GLOBAL"
+    #ROBOT_LIBRARY_VERSION = __version__
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def __init__(self):
+        pass
+
+    def repack_loose_files(self, sourceDir=DEFAULT_LOOSE_FILES_DIR, output_name="_TEST", deleteFiles=False, deleteFolders=False):
+        engine.repackAllLooseFiles(cwd=sourceDir, output_name=output_name, output_dir=DEFAULT_CONTENTPACKS_DIR,
+            delete_file_mode=deleteFiles, delete_folder_mode=deleteFolders)
